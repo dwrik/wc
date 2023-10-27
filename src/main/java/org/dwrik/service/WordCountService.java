1a338818f850c0aa3c8131b744b4f8ac3c78d3fa
dwrik <wrikdas22@gmail.com>
2023-10-27 13:16:13 +0530

    refactored WordCountService to handle passed in BufferedReader and made it generic

--- src/main/java/org/dwrik/service/WordCountService.java
+++ src/main/java/org/dwrik/service/WordCountService.java
@@ -1,55 +1,40 @@
 package org.dwrik.service;
 
-import org.dwrik.model.WordCount;
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+
+import org.dwrik.model.WordCount;
 
 public class WordCountService {
 
-  private final File file;
+    private final BufferedReader reader;
 
-  private final boolean showBytes;
-
-  private final boolean showLines;
-
-  private final boolean showWords;
-
-  private final boolean showChars;
-
-  public WordCountService(
-      File file, boolean showLines, boolean showWords, boolean showBytes, boolean showChars) {
-    this.file = file;
-    this.showLines = showLines;
-    this.showWords = showWords;
-    this.showBytes = showBytes;
-    this.showChars = showChars;
-  }
-
-  public WordCount getCount() throws IOException {
-    int lineCount = 0;
-    int wordCount = 0;
-    int byteOrCharCount = 0;
-    Charset charset = showChars ? StandardCharsets.UTF_8 : StandardCharsets.ISO_8859_1;
-    try (BufferedReader br = Files.newBufferedReader(file.toPath(), charset)) {
-      int c;
-      boolean prevCharSpace = true;
-      while ((c = br.read()) != -1) {
-        char ch = (char) c;
-        if (Character.isWhitespace(ch)) {
-          if (!prevCharSpace) wordCount++;
-          if (ch == '\n') lineCount++;
-          prevCharSpace = true;
-        } else if (ch != '\n') {
-          prevCharSpace = false;
-        }
-        byteOrCharCount++;
-      }
+    public WordCountService(BufferedReader reader) {
+        this.reader = reader;
     }
-    return new WordCount(lineCount, wordCount, byteOrCharCount);
-  }
+
+    public WordCount getCount() throws IOException {
+        int lineCount = 0;
+        int wordCount = 0;
+        int byetOrCharCount = 0;
+
+        int input = 0;
+        boolean isPrevCharSpace = true;
+
+        while ((input = reader.read()) != -1) {
+            char character = (char) input;
+
+            if (Character.isWhitespace(character)) {
+                wordCount += !isPrevCharSpace ? 1 : 0;
+                lineCount += character == '\n' ? 1 : 0;
+                isPrevCharSpace = true;
+            } else if (character != '\n') {
+                isPrevCharSpace = false;
+            }
+
+            byetOrCharCount++;
+        }
+
+        return new WordCount(lineCount, wordCount, byetOrCharCount);
+    }
 }
